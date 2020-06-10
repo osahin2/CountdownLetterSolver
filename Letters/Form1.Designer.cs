81f1d7fb50efa536acce73630e018a91b066215a
osahin2 <osahnn@outlook.com>
2020-06-10 18:53:12 +0300

    Update

---
+++ Letters/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Letters
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.harfBir_btn = new System.Windows.Forms.Button();
+            this.harfDokuz_btn = new System.Windows.Forms.Button();
+            this.harfSekiz_btn = new System.Windows.Forms.Button();
+            this.harfYedi_btn = new System.Windows.Forms.Button();
+            this.harfAlti_btn = new System.Windows.Forms.Button();
+            this.harfBes_btn = new System.Windows.Forms.Button();
+            this.harfDort_btn = new System.Windows.Forms.Button();
+            this.harfUc_btn = new System.Windows.Forms.Button();
+            this.harfIkı_btn = new System.Windows.Forms.Button();
+            this.Puan_lbl = new System.Windows.Forms.Label();
+            this.Kelimeler_lst = new System.Windows.Forms.ListBox();
+            this.Exit_btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.KelimeBul_btn = new System.Windows.Forms.Button();
+            this.Joker_btn = new System.Windows.Forms.Button();
+            this.Random_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // harfBir_btn
+            // 
+            this.harfBir_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.harfBir_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.harfBir_btn.Location = new System.Drawing.Point(47, 33);
+            this.harfBir_btn.Name = "harfBir_btn";
+            this.harfBir_btn.Size = new System.Drawing.Size(33, 33);
+            this.harfBir_btn.TabIndex = 0;
+            this.harfBir_btn.UseVisualStyleBackColor = false;
+            // 
+            // harfDokuz_btn
+            // 
+            this.harfDokuz_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.harfDokuz_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.harfDokuz_btn.Location = new System.Drawing.Point(367, 33);
+            this.harfDokuz_btn.Name = "harfDokuz_btn";
+            this.harfDokuz_btn.Size = new System.Drawing.Size(33, 33);
+            this.harfDokuz_btn.TabIndex = 2;
+            this.harfDokuz_btn.UseVisualStyleBackColor = false;
+            // 
+            // harfSekiz_btn
+            // 
+            this.harfSekiz_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.harfSekiz_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.harfSekiz_btn.Location = new System.Drawing.Point(327, 33);
+            this.harfSekiz_btn.Name = "harfSekiz_btn";
+            this.harfSekiz_btn.Size = new System.Drawing.Size(33, 33);
+            this.harfSekiz_btn.TabIndex = 3;
+            this.harfSekiz_btn.UseVisualStyleBackColor = false;
+            // 
+            // harfYedi_btn
+            // 
+            this.harfYedi_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.harfYedi_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.harfYedi_btn.Location = new System.Drawing.Point(287, 33);
+            this.harfYedi_btn.Name = "harfYedi_btn";
+            this.harfYedi_btn.Size = new System.Drawing.Size(33, 33);
+            this.harfYedi_btn.TabIndex = 4;
+            this.harfYedi_btn.UseVisualStyleBackColor = false;
+            // 
+            // harfAlti_btn
+            // 
+            this.harfAlti_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.harfAlti_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.harfAlti_btn.Location = new System.Drawing.Point(247, 33);
+            this.harfAlti_btn.Name = "harfAlti_btn";
+            this.harfAlti_btn.Size = new System.Drawing.Size(33, 33);
+            this.harfAlti_btn.TabIndex = 5;
+            this.harfAlti_btn.UseVisualStyleBackColor = false;
+            // 
+            // harfBes_btn
+            // 
+            this.harfBes_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.harfBes_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.harfBes_btn.Location = new System.Drawing.Point(207, 33);
+            this.harfBes_btn.Name = "harfBes_btn";
+            this.harfBes_btn.Size = new System.Drawing.Size(33, 33);
+            this.harfBes_btn.TabIndex = 6;
+            this.harfBes_btn.UseVisualStyleBackColor = false;
+            // 
+            // harfDort_btn
+            // 
+            this.harfDort_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.harfDort_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.harfDort_btn.Location = new System.Drawing.Point(167, 33);
+            this.harfDort_btn.Name = "harfDort_btn";
+            this.harfDort_btn.Size = new System.Drawing.Size(33, 33);
+            this.harfDort_btn.TabIndex = 7;
+            this.harfDort_btn.UseVisualStyleBackColor = false;
+            // 
+            // harfUc_btn
+            // 
+            this.harfUc_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.harfUc_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.harfUc_btn.Location = new System.Drawing.Point(127, 33);
+            this.harfUc_btn.Name = "harfUc_btn";
+            this.harfUc_btn.Size = new System.Drawing.Size(33, 33);
+            this.harfUc_btn.TabIndex = 8;
+            this.harfUc_btn.UseVisualStyleBackColor = false;
+            // 
+            // harfIkı_btn
+            // 
+            this.harfIkı_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.harfIkı_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.harfIkı_btn.Location = new System.Drawing.Point(87, 33);
+            this.harfIkı_btn.Name = "harfIkı_btn";
+            this.harfIkı_btn.Size = new System.Drawing.Size(33, 33);
+            this.harfIkı_btn.TabIndex = 9;
+            this.harfIkı_btn.UseVisualStyleBackColor = false;
+            // 
+            // Puan_lbl
+            // 
+            this.Puan_lbl.AutoSize = true;
+            this.Puan_lbl.BackColor = System.Drawing.Color.SandyBrown;
+            this.Puan_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Puan_lbl.Location = new System.Drawing.Point(557, 293);
+            this.Puan_lbl.Name = "Puan_lbl";
+            this.Puan_lbl.Size = new System.Drawing.Size(0, 39);
+            this.Puan_lbl.TabIndex = 15;
+            // 
+            // Kelimeler_lst
+            // 
+            this.Kelimeler_lst.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.Kelimeler_lst.FormattingEnabled = true;
+            this.Kelimeler_lst.ItemHeight = 16;
+            this.Kelimeler_lst.Location = new System.Drawing.Point(426, 33);
+            this.Kelimeler_lst.Name = "Kelimeler_lst";
+            this.Kelimeler_lst.Size = new System.Drawing.Size(188, 196);
+            this.Kelimeler_lst.TabIndex = 16;
+            this.Kelimeler_lst.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Exit_btn
+            // 
+            this.Exit_btn.BackColor = System.Drawing.Color.White;
+            this.Exit_btn.Image = global::Letters.Properties.Resources.icons8_delete_50px;
+            this.Exit_btn.Location = new System.Drawing.Point(633, 12);
+            this.Exit_btn.Name = "Exit_btn";
+            this.Exit_btn.Size = new System.Drawing.Size(75, 57);
+            this.Exit_btn.TabIndex = 17;
+            this.Exit_btn.UseVisualStyleBackColor = false;
+            this.Exit_btn.Click += new System.EventHandler(this.Exit_btn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::Letters.Properties.Resources.icons8_sign_pointing_right_100px;
+            this.pictureBox1.Location = new System.Drawing.Point(426, 254);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(101, 115);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // KelimeBul_btn
+            // 
+            this.KelimeBul_btn.Image = global::Letters.Properties.Resources.icons8_search_50px;
+            this.KelimeBul_btn.Location = new System.Drawing.Point(127, 236);
+            this.KelimeBul_btn.Name = "KelimeBul_btn";
+            this.KelimeBul_btn.Size = new System.Drawing.Size(193, 65);
+            this.KelimeBul_btn.TabIndex = 13;
+            this.KelimeBul_btn.UseVisualStyleBackColor = true;
+            this.KelimeBul_btn.Click += new System.EventHandler(this.KelimeBul_btn_Click);
+            // 
+            // Joker_btn
+            // 
+            this.Joker_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.Joker_btn.Enabled = false;
+            this.Joker_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Joker_btn.Image = global::Letters.Properties.Resources.icons8_joker_dc_50px;
+            this.Joker_btn.Location = new System.Drawing.Point(207, 79);
+            this.Joker_btn.Name = "Joker_btn";
+            this.Joker_btn.Size = new System.Drawing.Size(33, 71);
+            this.Joker_btn.TabIndex = 12;
+            this.Joker_btn.UseVisualStyleBackColor = false;
+            this.Joker_btn.Click += new System.EventHandler(this.Joker_btn_Click);
+            // 
+            // Random_btn
+            // 
+            this.Random_btn.Image = global::Letters.Properties.Resources.icons8_triple_arrows_50px;
+            this.Random_btn.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Random_btn.Location = new System.Drawing.Point(87, 156);
+            this.Random_btn.Name = "Random_btn";
+            this.Random_btn.Size = new System.Drawing.Size(273, 64);
+            this.Random_btn.TabIndex = 11;
+            this.Random_btn.Text = "Random Harf";
+            this.Random_btn.UseVisualStyleBackColor = true;
+            this.Random_btn.Click += new System.EventHandler(this.Random_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(706, 401);
+            this.Controls.Add(this.Exit_btn);
+            this.Controls.Add(this.Kelimeler_lst);
+            this.Controls.Add(this.Puan_lbl);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.KelimeBul_btn);
+            this.Controls.Add(this.Joker_btn);
+            this.Controls.Add(this.Random_btn);
+            this.Controls.Add(this.harfIkı_btn);
+            this.Controls.Add(this.harfUc_btn);
+            this.Controls.Add(this.harfDort_btn);
+            this.Controls.Add(this.harfBes_btn);
+            this.Controls.Add(this.harfAlti_btn);
+            this.Controls.Add(this.harfYedi_btn);
+            this.Controls.Add(this.harfSekiz_btn);
+            this.Controls.Add(this.harfDokuz_btn);
+            this.Controls.Add(this.harfBir_btn);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button harfBir_btn;
+        private System.Windows.Forms.Button harfDokuz_btn;
+        private System.Windows.Forms.Button harfSekiz_btn;
+        private System.Windows.Forms.Button harfYedi_btn;
+        private System.Windows.Forms.Button harfAlti_btn;
+        private System.Windows.Forms.Button harfBes_btn;
+        private System.Windows.Forms.Button harfDort_btn;
+        private System.Windows.Forms.Button harfUc_btn;
+        private System.Windows.Forms.Button harfIkı_btn;
+        private System.Windows.Forms.Button Random_btn;
+        private System.Windows.Forms.Button Joker_btn;
+        private System.Windows.Forms.Button KelimeBul_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Puan_lbl;
+        private System.Windows.Forms.ListBox Kelimeler_lst;
+        private System.Windows.Forms.Button Exit_btn;
+    }
+}
+
---
+++ Letters/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Letters
+{
+    public partial class Form1 : Form
+    {
+        public static int puan = 0;
+        public static Random rnd = new Random();
+        public static string[] bulunanKelimeler = new string[200];
+        public static int k = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public static void GetRandomString(Button btn)
+        {
+            string harfler = "abcçdefgğhıijklmnoöpqrsştuüvyz";
+            int num = rnd.Next(0, harfler.Length - 1);
+            btn.Text = harfler[num].ToString();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Exit_btn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void Random_btn_Click(object sender, EventArgs e)
+        {
+            puan = 0;
+            Button[] butonlar =
+                {harfBir_btn,harfIkı_btn,harfUc_btn,harfDort_btn,harfBes_btn,harfAlti_btn,harfYedi_btn,harfSekiz_btn,harfDokuz_btn
+            };
+            for(int i = 0; i < butonlar.Length; i++)
+            {
+                GetRandomString(butonlar[i]);
+            }
+        }
+
+        private void Joker_btn_Click(object sender, EventArgs e)
+        {
+            Button[] buttons =
+                {harfBir_btn,harfIkı_btn,harfUc_btn,harfDort_btn,harfBes_btn,harfAlti_btn,harfYedi_btn,harfSekiz_btn,harfDokuz_btn
+            };
+            int num = rnd.Next(1, 9);
+            GetRandomString(buttons[num]);
+            Joker_btn.Enabled = false;
+        }
+
+        private void KelimeBul_btn_Click(object sender, EventArgs e)
+        {
+            Kelimeler_lst.Items.Clear();
+
+            String[] Input = {
+                harfBir_btn.Text,harfIkı_btn.Text,harfUc_btn.Text,harfDort_btn.Text,harfBes_btn.Text,harfAlti_btn.Text,harfYedi_btn.Text,harfSekiz_btn.Text,harfDokuz_btn.Text
+            };
+            string dosya_yolu = @"D:\Dersler\Yazılım Yapımı\kelime-listesi.txt";
+            string[] wordList = System.IO.File.ReadAllLines(dosya_yolu, Encoding.UTF8);
+
+            HashSet<String> s_Words = new HashSet<String>(wordList);
+
+            Dictionary<String, String[]> s_Dict = s_Words
+                .Select(word => new
+                {
+                    Key = String.Concat(word.OrderBy(c => c)),
+                    Value = word
+                })
+                .GroupBy(item => item.Key, item => item.Value)
+                .ToDictionary(chunk => chunk.Key, chunk => chunk.ToArray());
+
+            string source = String.Concat(Input.OrderBy(c => c));
+
+            var result = Enumerable
+                .Range(1, (1 << source.Length) - 1)
+                .Select(index => string.Concat(source.Where((item, idx) => ((1 << idx) & index) != 0)))
+                .SelectMany(key =>
+                {
+                    String[] words;
+                    if (s_Dict.TryGetValue(key, out words))
+                        return words;
+                    else
+                        return new String[0];
+                })
+                .Distinct()
+                .OrderBy(word => word);
+            foreach (string s in result)
+            {
+                switch (s.Length)
+                {
+                    case 3:
+                        puan += 3;
+                        break;
+                    case 4:
+                        puan += 4;
+                        break;
+                    case 5:
+                        puan += 5;
+                        break;
+                    case 6:
+                        puan += 7;
+                        break;
+                    case 7:
+                        puan += 9;
+                        break;
+                    case 8:
+                        puan += 11;
+                        break;
+                    case 9:
+                        puan += 15;
+                        break;
+                    default:
+                        break;
+                }
+                Kelimeler_lst.Items.Add(s);
+            }
+            
+            Puan_lbl.Text = puan.ToString();
+
+            if (Kelimeler_lst.Items.Count == 0)
+                Joker_btn.Enabled = true;
+        }
+    }
+}
